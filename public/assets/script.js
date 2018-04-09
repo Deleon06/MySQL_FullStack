c5dfbac0e6d39db0c14a35ff66775b5c93a2c019
Gabriel <gabrieldeleon-ny@hotmail.com>
2018-04-08 22:17:41 -0400

    not devoring burger

---
+++ public/assets/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    
+    $(".devour-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var burger_id = $(this).children(".burger_id").val();
+    console.log(burger_id)
+    $.ajax({
+        method: "PUT",
+        url: "/burgers/" + burger_id
+    }).done(function(data) {
+        location.reload();
+    })
+
+    })
+});
--- server.js
+++ server.js
@@ -3,3 +3,18 @@ var bodyParser = require("body-parser");
 var methodOverride = require('method-override');
 var PORT = process.env.PORt || 5000;
 var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
